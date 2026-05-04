42d45ac7a9b4f495efa0de25a6a0ccc651c090ff
KNG2K03 <kkarundu8@gmail.com>
2026-05-04 19:09:27 +0100

    added mongoose to server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // imports 
 const express = require('express')
 const body_parser = require('body-parser')
+const mdb = require('mongoose')
 
 // express 
 const app = express()
@@ -9,6 +10,15 @@ const port = 4000
 // body parser
 app.use(body_parser.urlencoded({ extended: false }));
 app.use(body_parser.json());
+
+// mongoose
+mongodb_connect().catch((err) => { console.log(err) })
+
+// connecting to database
+async function mongodb_connect() {
+    await mdb.connect("mongodb+srv://hello:there@datarepdb.fhnaepc.mongodb.net/?appName=DataRepDB")
+    console.log('Database Connected!\n')
+}
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}...`)
