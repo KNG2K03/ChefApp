799b04e612e752a98396a37af384e5c0d3fd9b3e
KNG2K03 <kkarundu8@gmail.com>
2026-05-04 19:14:56 +0100

    added remove recipe put method in server for when a user removes a recipe from their collection

--- backend/server.js
+++ backend/server.js
@@ -129,6 +129,28 @@ app.put('/addRecipe', async (req,res) => {
 
 })
 
+app.put('/removeRecipe', async (req, res) => {
+    let collection_name = req.body.cname
+    let recipe_id = req.body.rid 
+
+    // get chef from database 
+    let chef = await Chef.findById(active_chef_id)
+
+    for(let i = 0; i <= chef.recipe_book.length - 1; i++) {
+        if (chef.recipe_book[i].name === collection_name) {
+            chef.recipe_book[i].meals.pull(recipe_id); 
+            break;
+        }
+    }
+
+    chef.save()
+
+    res.status(200).json({
+        message: `Successfully removed ${recipe_id} to ${collection_name}`
+    })
+
+})
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}...`)
     console.log(`Go to http://localhost:${port}`)
