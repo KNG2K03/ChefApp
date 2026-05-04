dd065d56e4ae876dd369d329218e65689926d8a4
KNG2K03 <kkarundu8@gmail.com>
2026-05-04 19:08:04 +0100

    added body parser to server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 // imports 
 const express = require('express')
+const body_parser = require('body-parser')
 
 // express 
 const app = express()
 const port = 4000
+
+// body parser
+app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.json());
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}...`)
