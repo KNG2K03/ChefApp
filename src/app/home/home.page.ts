b2a6c41670946bf3611d4472dea489290a7437cd
KNG2K03 <kkarundu8@gmail.com>
2026-05-05 13:16:22 +0100

    create foundation of mealcard

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { MealCardComponent } from '../meal-card/meal-card.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { MealTagComponent } from '../meal-tag/meal-tag.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, MealCardComponent, MealTagComponent],
 })
 export class HomePage {
   constructor() {}
--- src/app/meal-card/meal-card.component.ts
+++ src/app/meal-card/meal-card.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { MealTagComponent } from '../meal-tag/meal-tag.component';
+import { IonCard, IonHeader, IonCardTitle, IonCardSubtitle, IonContent, IonCardContent, IonCardHeader } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-meal-card',
   templateUrl: './meal-card.component.html',
   styleUrls: ['./meal-card.component.scss'],
+  imports: [MealTagComponent, IonCard, IonHeader, IonCardTitle, IonCardSubtitle, IonContent, IonCardContent, IonCardHeader]
 })
 export class MealCardComponent  implements OnInit {
 
   constructor() { }
 
   ngOnInit() {}
+
+  meal_info = {
+    meal_name:  "meal name",
+    meal_image: "https://www.themealdb.com/images/media/meals/sutysw1468247559.jpg",
+    meal_category: "meal category",
+    meal_area: "meal area"
+  }
 
 }
