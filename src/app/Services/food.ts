0f0385fe84d0d7440f7c0a55ba8b970f478e86de
KNG2K03 <kkarundu8@gmail.com>
2026-05-11 10:43:30 +0100

    added food service and functionality within it

--- src/app/Services/food.ts
+++ src/app/Services/food.ts
@@ -1,8 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Food {
-  
+export class FoodService {
+  constructor (private http: HttpClient) { }
+
+  search_food(search: any):Observable<any> {
+    return this.http.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+  }
+
+  search_by_id(id: any):Observable<any> {
+    return this.http.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+  }
+
+  search_by_country(country: any):Observable<any> {
+    return this.http.get(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${country}`)
+  }
 }
