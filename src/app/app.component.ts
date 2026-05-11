055227b0cc85eb4c874f08fd41e48e5bdeb0cfb1
KNG2K03 <kkarundu8@gmail.com>
2026-05-11 12:13:56 +0100

    removed unused imports from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet, IonTabs, IonTab, IonHeader, IonToolbar, IonTitle, IonContent, IonTabBar, IonTabButton, IonIcon } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon } from '@ionic/angular/standalone';
 
 import { addIcons } from 'ionicons';
 import { home, search, person } from 'ionicons/icons';
@@ -7,7 +7,7 @@ import { home, search, person } from 'ionicons/icons';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, IonTabs, IonTab, IonHeader, IonToolbar, IonTitle, IonContent, IonTabBar, IonTabButton, IonIcon],
+  imports: [IonApp, IonRouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon],
 })
 export class AppComponent {
   constructor() {
