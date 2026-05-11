402b59214a6813142cf980e924c3ae091556229c
KNG2K03 <kkarundu8@gmail.com>
2026-05-11 10:46:06 +0100

    moved components to components folder

---
+++ src/app/components/long-meal-card/long-meal-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-long-meal-card',
+  templateUrl: './long-meal-card.component.html',
+  styleUrls: ['./long-meal-card.component.scss'],
+})
+export class LongMealCardComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/components/meal-card/meal-card.component.ts
@@ -0,0 +1,27 @@
+import { Component, input, OnInit } from '@angular/core';
+import { FoodService } from '../../Services/food';
+import { IonCard, IonCardTitle, IonCardSubtitle, IonContent, IonCardContent, IonCardHeader } from "@ionic/angular/standalone";
+
+
+@Component({
+  selector: 'app-meal-card',
+  templateUrl: './meal-card.component.html',
+  styleUrls: ['./meal-card.component.scss'],
+  imports: [IonCard, IonCardTitle, IonCardSubtitle, IonCardHeader]
+})
+export class MealCardComponent implements OnInit {
+
+  meal_name = input<string>();
+  meal_image = input<string>();
+  meal_area = input<string>();
+  meal_category = input<string>();
+
+  constructor() { 
+    
+  }
+
+  ngOnInit() { }
+
+  
+
+}
