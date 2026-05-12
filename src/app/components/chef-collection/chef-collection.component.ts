a168142ba93a4f950c80d7e82005efb3f012712b
KNG2K03 <kkarundu8@gmail.com>
2026-05-12 14:36:55 +0100

    finish desinging collection button

--- src/app/components/chef-collection/chef-collection.component.ts
+++ src/app/components/chef-collection/chef-collection.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chef-collection',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chef-collection.component.scss'],
 })
 export class ChefCollectionComponent  implements OnInit {
+
+  collection_name = input<string>();
+  amount_of_meals = input<string>();
+  
 
   constructor() { }
 
